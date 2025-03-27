ae7a21fa353007e44f6a58013c60c7ad008af2ae
Travis Nyland <122824127+HelloPingPong@users.noreply.github.com>
2025-03-27 14:13:28 -0400

    Create SchemaSummaryDto.java

---
+++ src/main/java/com/example/datagenerator/dto/SchemaSummaryDto.java
@@ -0,0 +1,13 @@
+package com.example.datagenerator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SchemaSummaryDto {
+    private Long id;
+    private String name;
+}
