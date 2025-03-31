91aa2caecae83df05bc2aa3b4d50b47c1033f35b
Travis Nyland <122824127+HelloPingPong@users.noreply.github.com>
2025-03-31 13:21:48 -0400

    Create TrueFalseGenerator.java

---
+++ src/main/java/com/example/datagenerator/generator/impl/TrueFalseGenerator.java
@@ -0,0 +1,29 @@
+package com.example.datagenerator.generator.impl;
+
+import com.example.datagenerator.generator.DataTypeGenerator;
+import com.github.javafaker.Faker;
+import org.springframework.stereotype.Component;
+
+@Component // Register as a Spring bean
+public class TrueFalseGenerator implements DataTypeGenerator {
+
+    @Override
+    public String getKey() {
+        return "Boolean.trueFalse";
+    }
+
+    @Override
+    public String generate(Faker faker, String options) {
+        return faker.bool().bool() ? "True" : "False";
+    }
+
+    @Override
+    public String getName() {
+        return "True/False";
+    }
+
+    @Override
+    public String getCategory() {
+        return "Boolean";
+    }
+}
