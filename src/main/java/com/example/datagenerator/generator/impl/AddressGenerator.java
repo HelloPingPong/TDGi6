78c9d424d3b77fd99417e04b63eb2cf96c062101
Travis Nyland <122824127+HelloPingPong@users.noreply.github.com>
2025-03-31 13:14:47 -0400

    Create AddressGenerator.java

---
+++ src/main/java/com/example/datagenerator/generator/impl/AddressGenerator.java
@@ -0,0 +1,29 @@
+package com.example.datagenerator.generator.impl;
+
+import com.example.datagenerator.generator.DataTypeGenerator;
+import com.github.javafaker.Faker;
+import org.springframework.stereotype.Component;
+
+@Component // Register as a Spring bean
+public class AddressGenerator implements DataTypeGenerator {
+
+    @Override
+    public String getKey() {
+        return "Address.streetAddress";
+    }
+
+    @Override
+    public String generate(Faker faker, String options) {
+        return faker.address().streetAddress();
+    }
+
+    @Override
+    public String getName() {
+        return "Street Address";
+    }
+
+    @Override
+    public String getCategory() {
+        return "Address";
+    }
+}
