99c6c4bf8ae782008a7fa2cf87ccbdfa7c51b122
Travis Nyland <122824127+HelloPingPong@users.noreply.github.com>
2025-05-09 13:00:26 -0400

    Create footer.jsx

---
+++ reactoption/claude1/footer.jsx
@@ -0,0 +1,13 @@
+// src/components/Footer/Footer.jsx
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="app-footer">
+      <p>Backend: Spring Boot | Frontend: React</p>
+    </footer>
+  );
+};
+
+export default Footer;
